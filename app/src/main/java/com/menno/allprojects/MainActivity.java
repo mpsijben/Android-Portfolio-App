d4afb174aaf1c7c1989640ed411ab7a7d0dccb11
Menno Sijben <mennosijben@gmail.com>
2015-11-19 22:24:11 +0100

    Changed the hex colors to the resource file, made the events cleaner/smaller and made layout to a LinearLayout

--- app/src/main/java/com/menno/allprojects/MainActivity.java
+++ app/src/main/java/com/menno/allprojects/MainActivity.java
@@ -1,10 +1,9 @@
 package com.menno.allprojects;
 
 import android.content.Context;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -18,54 +17,21 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
 
 
-        final Button button = (Button) findViewById(R.id.btn_spotify);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Context context = getApplicationContext();
-                Toast toast = Toast.makeText(context, "This button will launch my spotify streamer app!", Toast.LENGTH_SHORT);
-                toast.show();
-            }
-        });
-        final Button button2 = (Button) findViewById(R.id.btn_scores);
-        button2.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Context context = getApplicationContext();
-                Toast toast = Toast.makeText(context, "This button will launch my scores app app!", Toast.LENGTH_SHORT);
-                toast.show();
-            }
-        });
-        final Button button3 = (Button) findViewById(R.id.btn_library);
-        button3.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Context context = getApplicationContext();
-                Toast toast = Toast.makeText(context, "This button will launch my library app!", Toast.LENGTH_SHORT);
-                toast.show();
-            }
-        });
-        final Button button4 = (Button) findViewById(R.id.btn_build_bigger);
-        button4.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Context context = getApplicationContext();
-                Toast toast = Toast.makeText(context, "This button will launch my build it bigger app!", Toast.LENGTH_SHORT);
-                toast.show();
-            }
-        });
-        final Button button5 = (Button) findViewById(R.id.btn_xyz_reader);
-        button5.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Context context = getApplicationContext();
-                Toast toast = Toast.makeText(context, "This button will launch my xyz reader app!", Toast.LENGTH_SHORT);
-                toast.show();
-            }
-        });
-        final Button button6 = (Button) findViewById(R.id.btn_capstone);
-        button6.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Context context = getApplicationContext();
-                Toast toast = Toast.makeText(context, "This button will launch my capstone app!", Toast.LENGTH_SHORT);
-                toast.show();
-            }
-        });
+
+    }
+
+    public void displayToast(View view) {
+
+        Button button = (Button) view;
+        String buttonText = (String) button.getText();
+        Context context = getApplicationContext();
+
+        CharSequence text = getString(R.string.open_app)
+                + buttonText;
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 
     @Override
